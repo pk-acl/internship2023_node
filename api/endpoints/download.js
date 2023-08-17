4beb002ecc1cb930aa4d31305ae2ff9f351345db
prabhakaran.m <prabhakaran.m@acldigital.com>
2023-08-17 16:01:25 +0530

    cors

--- api/endpoints/download.js
+++ api/endpoints/download.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router()
+const cors = require('cors');
+router.use(cors());
 
 const userController = require('../controllers/download');
 
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { default: mongoose } = require('mongoose');
 
 const PORT = 8080;
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
